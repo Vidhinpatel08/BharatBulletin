859fe7ec4140ad00bce9931373f1f75531fe5ce9
Vidhinpatel08 <vidhin1208@gmail.com>
2024-03-29 18:14:33 +0530

    Update for mobile device and some colors

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
+import defaultLogo from '../BharatBulletin_logo.png'
 
 const NewsItem = (props) => {
     let { title, desc, imageUrl, newsUrl, summary, date, author, source } = props
     let [fullArticle, setFullArticle] = useState(false);
-    var defaultimage = ("https://media.istockphoto.com/id/547356494/video/loading-symbol-loop.jpg?s=640x640&k=20&c=TU113GZc5CUv3yC0wAgY94Um6hbedSbs0i58jTi7Nx8=");
     const readMoreData = (e) => {
         setFullArticle(true)
     }
@@ -11,19 +11,12 @@ const NewsItem = (props) => {
     return (
         <div className='my-3'>
             <div className="card mx-2 border border-dark Card-Box " >
-                <div style={{
-                    display: 'flex',
-                    justifyContent: 'flex-end',
-                    right: '-10px',
-                    top: '-10px',
-                    position: 'absolute',
-                    fontSize: '18px'
-                }}>
+                <div className='newscard-tagbar'>
                     <span className="badge rounded-pill bg-danger select-none" style={{ zIndex: '1', left: '90%' }}>{source.split('.')[0]}</span>
                 </div>
 
                 <div className="img-container select-none">
-                    <img src={imageUrl !== 'default' ? imageUrl : defaultimage} className="card-img-top" alt="loading" width={400} height={220} />
+                    <img src={imageUrl !== 'default' ? imageUrl : defaultLogo} className="card-img-top" alt="loading" width={400} height={220} />
                 </div>
                 <div className="card-body">
                     <h5 className="card-title fw-semibold mukta-vaani-bold">{title}</h5>
